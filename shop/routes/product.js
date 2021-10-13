ab4a8d19d9aa7c2674cbd1dce082dbc728e1a542
veerapandi2100 <90551639+veerapandi2100@users.noreply.github.com>
2021-10-13 15:38:02 +0530

    Update product.js
    Product list and product details API Added

--- shop/routes/product.js
+++ shop/routes/product.js
@@ -9,6 +9,8 @@ const router = express.Router();
 const Joi = require('@hapi/joi');
 const productdata = require('../models/product');
 const logger = require('../config/logfile');
+const Sequelize = require('sequelize');
+const Op = Sequelize.Op;
 
 // Product create api
 router.post('/create', (req, res) => {
@@ -67,6 +69,62 @@ router.post('/create', (req, res) => {
     });   
 })
 
+// Product list api
+router.get('/product_list', (req, res) => {
+    //  console.log('user==>', req.userId);
+      const limitData = +req.query.limit ? +req.query.limit: null;
+      const offsetData = +req.query.offset ? +req.query.offset: null;
+      const sort = (req.query.sort == 1) ?  'DESC': 'ASC'; 
+      const search = req.query.search;
+    //  console.log('user==>',limitData, req.query.search);
+    let whereData = {};
+    whereData = { product_name: {[Op.like]: `%${search}%`}, order: [['product_id', sort]], offset: offsetData, limit: limitData };
+    productdata.findAll(whereData).then((result) => { 
+          const output = result.map((data) => {
+              const objectData = {};
+              objectData.productId = data.product_id;
+              objectData.productName = data.product_name;
+              objectData.productImagePath = data.product_image_path;
+              objectData.productUnicode = data.product_unicode;
+              objectData.productQuantity = data.product_quantity;
+              objectData.productPrice= data.product_price;
+              objectData.createdUserId = data.created_by ? data.created_by : 0;
+              objectData.createdDate = data.createdAt;
+              return objectData;
+          });
+             logger.info("Product list show successfully"); 
+             return res.status(200).send({status: 1, message: "Product list show successfully", data: output});            
+          }).catch(function(err) {
+              // print the error details
+              logger.error("error", err);
+          });
+  });
+
+// Get Product product details api
+router.get('/get_product', (req, res) => {
+    //  console.log('user==>', req.userId);
+    const productId = (req.query.productId != "") ? req.query.productId : 0;
+    productdata.findByPk(productId).then((result) => {
+        if(!result) {
+            logger.error("give a valid product id");
+            return res.status(503).send({status: 0, message: 'Give a valid product id'});
+        }
+        const outResult = {};
+        outResult.productId = result.product_id;
+        outResult.productName = result.product_name;
+        outResult.productImagePath = result.product_image_path;
+        outResult.productUnicode = result.product_unicode;
+        outResult.productQuantity = result.product_quantity;
+        outResult.productPrice = result.product_price;
+        outResult.createdBy = result.created_by;
+        outResult.createdDate = result.createdAt;
+        logger.info("Product details show successfully"); 
+        return res.status(200).send({status: 1, message: "Product details show successfully", data: outResult});  
+    }).catch(function(err) {
+        // print the error details
+        logger.error("error", err);
+    });      
+});
 
 
 module.exports = router;
